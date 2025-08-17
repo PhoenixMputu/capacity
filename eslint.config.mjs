6be68b5d7cb883deca1bc15809ab0c2e80e4b556
Victory Deo <victory23tshimbadi@gmail.com>
2025-08-17 06:14:56 +0100

    chore(eslint): enhance config with additional rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,46 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  
+  {
+    rules: {
+      "quotes": ["error", "single", { avoidEscape: true }],
+      "semi": ["error", "always"],
+      "no-console": "warn",
+      
+      "unused-imports/no-unused-imports": "error",
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index"
+          ],
+          "newlines-between": "always",
+          alphabetize: { order: "asc", caseInsensitive: true }
+        }
+      ]
+    }
+  },
+  
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module"
+    },
+    ignores: [
+      "node_modules/",
+      "dist/",
+      ".next/",
+      "out/",
+      "*.d.ts",
+      "*.env"
+    ]
+  }
 ];
 
 export default eslintConfig;
